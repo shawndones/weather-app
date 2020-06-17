dd5857508abb37b1540fbe898743c208e8adc603
Shawn Dones <shawndones@gmail.com>
2020-06-17 10:19:24 -0400

    update protocol

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Moment from 'react-moment';
+import moment from 'moment';
 // import WeatherList from './components/WeatherList';
 
 /*
@@ -13,7 +14,7 @@ const APIKEY = "84ef52b9160716fd0953ce92456fb897"
 
 export default class App extends Component {
   state = {
-    zip: "40107",
+    zip: "37738",
     currentTemp: "",
     weather: [],
     forecast: []
@@ -21,9 +22,9 @@ export default class App extends Component {
   // Making 2 api calls at once https://medium.com/@wisecobbler/using-the-javascript-fetch-api-f92c756340f0
 
   componentDidMount() {
-    var currentWeather = fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${this.state.zip},us&appid=${APIKEY}`)
+    var currentWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${this.state.zip},us&appid=${APIKEY}`)
     .then(response => response.json());
-    var forecastWeather = fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${this.state.zip},us&appid=${APIKEY}`)
+    var forecastWeather = fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${this.state.zip},us&appid=${APIKEY}`)
     .then(response => response.json());
     var combinedData = {"currentWeather":{},"forecastWeather":{}};
     Promise.all([currentWeather, forecastWeather])
@@ -33,10 +34,11 @@ export default class App extends Component {
         return combinedData;
       }).then(combinedData => {
         // console.log(combinedData["currentWeather"]);
+        // console.log(combinedData["forecastWeather"]);
         const { weather, main } = combinedData["currentWeather"];
         const { temp } = main;
         const { list } = combinedData["forecastWeather"];
-        // console.log(list);
+
         this.setState({
           currentTemp: temp,
           weather: weather[0],
@@ -45,7 +47,54 @@ export default class App extends Component {
       });
   }
 
+  getHighsLows = (list) => {
 
+        // let listKeys = Object.keys(list);
+        let listValues = Object.values(list);
+
+        // console.log(listValues);
+        let prevDay;
+        let dayTemps = [];
+        let i = 0;
+
+        for(const value of listValues) {
+          // console.log(previousDate + value.dt_txt);
+          let nowDay = new Date(value.dt_txt);
+          // console.log(nowDay);
+          nowDay = moment(nowDay).format('dddd');
+
+
+
+          // let nowMonth = nowDay.getDate();
+          console.log(nowDay)
+          // console.log(value.dt_txt);
+          // console.log(prevDay);
+          if((nowDay === prevDay) || (i === 0) ) {
+
+           
+            dayTemps.push(value.main.temp);
+            // console.log(value.main.temp);
+            console.log(dayTemps);
+            
+          } else {
+              // Stop interation and find low and high temp for this day
+              // console.log(nowDay);
+              // console.log(Math.max(...dayTemps));
+              // console.log(Math.floor(...dayTemps));
+
+              // Reset for new day
+              dayTemps = [];
+              i = 0;
+          }
+         
+          // const dates = [];
+        //  dates.push = value.dt_txt;
+        prevDay = new Date(value.dt_txt);
+        prevDay = nowDay;
+         i++;
+        }
+
+  }
   tempConvert = (temp) => {
     temp = (temp-273.15)* 9/5 + 32
     return Math.floor(temp)
@@ -57,15 +106,24 @@ export default class App extends Component {
     // console.log(this.state.weather.id);
     // console.log(this.state.forecast);
     // const fiveDayForecast = this.state.forecast.slice( 0, 5)
+    this.getHighsLows(this.state.forecast);
     return (
       <div className="weatherApp">
-        <h1>Weather in Boston, KY</h1>
+        <header className="c-header">
+          <h1>Weather</h1>
+        </header>
         <h2>Current Conditions: {currentConditions.main}</h2>
         <img src={`http://openweathermap.org/img/w/${currentConditions.icon}.png`} alt="" />
         <p>{temp}&#176;</p>
+
+        <h2>Highs &amp; Lows</h2>
+      
+        
         <h2>Forecast</h2>
         <div className="grid">
         {this.state.forecast.map((weatherItem,key) => (
+          
+
           <div
             className="day" 
             key={key}>
